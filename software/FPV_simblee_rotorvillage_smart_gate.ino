40ef5c68d181962f7aa3b83610721d7ca61f8340
Jerome <robomaniac@gmail.com>
2018-02-25 16:50:16 -0500

    updated simblee (arduino) code

--- software/FPV_simblee_rotorvillage_smart_gate.ino
+++ software/FPV_simblee_rotorvillage_smart_gate.ino
@@ -1,91 +1,67 @@
 /*
-
 Rotorvillage gate
-Servo controller using bluetooth
+Servo controller using bluetooth.
 
+Servo is connected to pin 2
+LED strip connected to pin 3
+Buzzer connected to pin 4 (not yet used in code)
 
 Jerome Demers
 January 2018
 
-
 to do:
+- DONE make the Start switch position base on witdh of phone and not hard coded (SimbleeForMobile.screenWidth/2)
 - make beeper go off when idle for X minutes. If nothing is actiaved, sound the alarm and reset when you trigger the START button
 - read voltage on 1S of the lipo to trigger alarm and shutdown system
-- remove all the variable that are not use
 - clean the code
-- prepare github
-- schematic
-
-
 
 */
 
 #include <Servo.h>
 #include <SimbleeForMobile.h>
-
-#include "rotorVILLAGE_png.h"
-
+#include "rotorVILLAGE_png.h"         // the logo was generated using this instructions https://www.youtube.com/watch?v=NglwMpqrTpI
 #define LOGO_DOT_IMAGE 1
 
 
 
 Servo s1;
-int nLevel = 1;
-
-int nStart=0;
-
-int ExpertState = LOW;             // ledState used to set the LED
-unsigned long previousMillis = 0;        // will store last time LED was updated
+int nLevel = 1;                       // skills level, 1 to 3
+int ExpertState = LOW;                // ledState used to set the LED
+unsigned long previousMillis = 0;     // will store last time LED was updated
 const long interval = 3000;           // interval at which to change motor direction in expert mode
 
+const int led = 3;                    // white LED ring is connected to pin 3
+const int buzzer = 4;
 
-// pin 3 on the RGB shield is the red led
-// (can be turned on/off from the iPhone app)
-int led = 3;
-
-// pin 5 on the RGB shield is button 1
-// (button press will be shown on the iPhone app)
-int button = 5;
-
-// debounce time (in ms)
-int debounce_time = 10;
-
-// maximum debounce timeout (in ms)
-int debounce_timeout = 100;
-
-
-int blackSwitch;
-int BeginnerSwitch;
+int BeginnerSwitch;                   // these are switchs and text for the user interface
 int NormalSwitch;
 int ExpertSwitch;
-int blackText;
+
 int BeginnerText;
 int NormalText;
 int ExpertText;
-int segment;
 
 int LEDText;
 int LEDSwitch;
 
-int MotorState = LOW;             // MotorState used to set the LED
-
-int ServoStop = 92;
-int ServoBeginner = 95;
+int MotorState = LOW;                 // MotorState used to set the LED
+  
+int ServoStop = 92;                   // value so the motor stop moving. It's the center position
+int ServoBeginner = 95;               // speed for all level
 int ServoNormal = 98;
 int ServoExpert = 110;
 
+int currentState, previousState = 0;
+uint8_t ui_start_button;                    // user interface START button
+int nExpertMode=0;                    // state needed to start the fwd and reverse motor motion
 
+
+//===============================================================================================
 void setup() {
-  //Serial.begin(9600);
- // Serial.println("Started");
-
-  s1.attach(2);   // servo motor
   
-  // led turned on/off from the iPhone app
-  pinMode(led, OUTPUT);
-
-  // button press will be shown on the iPhone app)
-  pinMode(button, INPUT);
+  s1.attach(2);   // servo motor on pin2 of simblee
+  pinMode(led, OUTPUT);  // led turned on/off from the iPhone app
+  pinMode(buzzer, OUTPUT); 
 
   // this is the data we want to appear in the advertisement
   // (if the deviceName and advertisementData are too long to fix into the 31 byte
@@ -100,67 +76,27 @@ void setup() {
   SimbleeForMobile.begin();
 }
 
-int debounce(int state)
-{
-  int start = millis();
-  int debounce_start = start;
-  
-  while (millis() - start < debounce_timeout)
-    if (digitalRead(button) == state)
-    {
-      if (millis() - debounce_start >= debounce_time)
-        return 1;
-    }
-    else
-      debounce_start = millis();
 
-  return 0;
-}
 
-int delay_until_button(int state)
-{
-  // set button edge to wake up on
-  if (state)
-    Simblee_pinWake(button, HIGH);
-  else
-    Simblee_pinWake(button, LOW);
-    
-  do
-    // switch to lower power mode until a button edge wakes us up
-    Simblee_ULPDelay(INFINITE);
-  while (! debounce(state));
-  
-  // if multiple buttons were configured, this is how you would determine what woke you up
-  if (Simblee_pinWoke(button))
-  {
-    // execute code here
-    Simblee_resetPinWake(button);
-  }
-}
 
-int currentState, previousState = 0;
-uint8_t ui_button;
-uint8_t ui_rect;
-int hello=0;
-
+//===============================================================================================
 void loop() {
   
-if(hello==1){ // move servo motor in expert mode, the interval and motor speed should be random
+if(nExpertMode==1){ // move servo motor in expert mode, the interval and motor speed should be random
   
-
-   unsigned long currentMillis = millis();
+  unsigned long currentMillis = millis();  
   
-  if (currentMillis - previousMillis >= interval) {
+  if (currentMillis - previousMillis >= interval) {  // this is where it wait 3 sec before the servo motor change direction
     
     previousMillis = currentMillis;
 
     // if the LED is off turn it on and vice-versa:
     if (ExpertState == LOW) {
       ExpertState = HIGH;
-      s1.write(96);
+      s1.write(96);   // these should be variable so I can randomly change them
     } else {
       ExpertState = LOW;
-      s1.write(86);
+      s1.write(86); // these should not be hardcoded
     }
 
   }// end if
@@ -169,10 +105,12 @@ if(hello==1){ // move servo motor in expert mode, the interval and motor speed s
 
   // process must be called in the loop for SimbleeForMobile
   SimbleeForMobile.process();
-}
+  
+}  //end loop
 
 
-
+//===============================================================================================
+// this is where you design and create the user interface
 void ui()
 {
 
@@ -190,11 +128,8 @@ void ui()
   SimbleeForMobile.beginScreen(0xFF112244);
   SimbleeForMobile.drawRect(0,0,SimbleeForMobile.screenWidth, SimbleeForMobile.screenHeight,  rgb(235,235,235));
   
+    //SimbleeForMobile.drawRect(19,  33, 285,  17,  0xFF112244);//rectangle parfait
   
-  //SimbleeForMobile.drawRect(19,  33, 285,  17,  0xFF112244);//rectangle parfait
-  
-
-
   // Rotorvillage logo
   SimbleeForMobile.imageSource(LOGO_DOT_IMAGE, rotorVILLAGEpanda_png_Type, rotorVILLAGEpanda_png, rotorVILLAGEpanda_png_len);
   SimbleeForMobile.drawImage(LOGO_DOT_IMAGE, 20, 60);
@@ -224,45 +159,44 @@ void ui()
   ExpertSwitch = SimbleeForMobile.drawSwitch(switchLeftMargin, topMargin);
   topMargin += deltaY+27;
 
-//------------------------------------------------------
-
   // light grey background
   SimbleeForMobile.drawRect(0,  topMargin-7,320 ,  40,  WHITE );
   LEDText = SimbleeForMobile.drawText(leftMargin, topMargin, "LED Light", BLACK, textSize);
   LEDSwitch = SimbleeForMobile.drawSwitch(switchLeftMargin, topMargin-3);
 
-
-
-  ui_button = SimbleeForMobile.drawButton(120, 510, 80, "START");
-  SimbleeForMobile.setEvents(ui_button, EVENT_PRESS | EVENT_RELEASE);  
-
-
+  ui_start_button = SimbleeForMobile.drawButton(SimbleeForMobile.screenWidth/2, 510, 80, "START");
+  SimbleeForMobile.setEvents(ui_start_button, EVENT_PRESS | EVENT_RELEASE);  
 
   SimbleeForMobile.endScreen();
-}
+  
+} // end ui
 
+//===============================================================================================
+// the moment you close the app, it goes here
 void SimbleeForMobile_onDisconnect()
 {
+    // we make sure we turn off everything when the app is disconnected
     digitalWrite(led, LOW);
     s1.write(ServoStop);
     nLevel=1; // make button at beginner mode before leving
     MotorState = LOW; // make sure the state is low so next time when you press the start button it trigger motor
     delay(300);
-    s1.detach(); 
+    s1.detach(); // wait before detach servo
 }
 
-
+//===============================================================================================
+// the moment you open the app it pass here
 void SimbleeForMobile_onConnect()
 {
-  s1.attach(2);   // servo motor
+  // you can do an action when the phone connects to the simblee
 }
 
+//===============================================================================================
+// this handle all the event related to the user interface. Each button, slider, etc are link to an event id
 void ui_event(event_t &event)
 {
 
-  
-
-  // turn on LED
+  // turn on LED strip 
   if (event.id == LEDSwitch) {
     if (event.value == 1)
       digitalWrite(led, HIGH);
@@ -271,41 +205,37 @@ void ui_event(event_t &event)
   }
 
 
-// start button
-  if (event.id == ui_button) {
+  // start button
+  if (event.id == ui_start_button) {
     if (event.type == EVENT_PRESS)     {
         // if the motor state is off turn it on and vice-versa:
         // I tried MotorState != MotorState but could not get it to work
-        if (MotorState == LOW) {
-          MotorState = HIGH;
-          } else {
-            MotorState = LOW;
-         }
+        if (MotorState == LOW)
+           MotorState = HIGH;
+        else
+           MotorState = LOW;
       } //end if
 
-    }// end event
+    }// end event.id
 
    
   // move the servo motor
   if(MotorState == HIGH){
-
-s1.attach(2);   // servo motor
-  
     
+    s1.attach(2);   // servo motor
+  
     if(nLevel==1)
       s1.write(ServoBeginner);
      if(nLevel==2)
       s1.write(ServoNormal);  
-     if(nLevel==3)
-        hello=1;
+     if(nLevel==3)  // go into expert mode
+        nExpertMode=1;
       else
-        hello=0;
-     // Expert();
-      
+        nExpertMode=0;
+
   }else
   {
     s1.detach(); 
-   // s1.write(ServoStop);// servo stop
   }
 
 
@@ -328,7 +258,7 @@ if (event.id == BeginnerSwitch && event.value == 1) {
     SimbleeForMobile.updateValue(NormalSwitch, 0);
     SimbleeForMobile.updateValue(ExpertSwitch, 1);
     
-   }  else if (event.id == BeginnerSwitch && event.value == 0) {   // this 3 if's are for when you press the same button, it won't let the button be off
+  }  else if (event.id == BeginnerSwitch && event.value == 0) {   // this 3 if's are for when you press the same button, it won't let the button be off
   
     SimbleeForMobile.updateValue(BeginnerSwitch, 1);
     SimbleeForMobile.updateValue(NormalSwitch, 0);
@@ -344,31 +274,10 @@ if (event.id == BeginnerSwitch && event.value == 1) {
     SimbleeForMobile.updateValue(NormalSwitch, 0);
     SimbleeForMobile.updateValue(ExpertSwitch, 1);
     
-   }
+  }
 
   
 } // end ui_event
-
-void Expert(){
-   unsigned long currentMillis = millis();
-  
-  if (currentMillis - previousMillis >= interval) {
-    
-    previousMillis = currentMillis;
-
-    // if the LED is off turn it on and vice-versa:
-    if (ExpertState == LOW) {
-      ExpertState = HIGH;
-      s1.write(99);
-    } else {
-      ExpertState = LOW;
-      s1.write(87);
-    }
-
-  }// end if
-}// end expert
-
-
 
 
 
